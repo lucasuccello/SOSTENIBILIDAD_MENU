8becd0f494235ab5c5893d65189d0d014547a1f3
Jose Lucero <jlucero@softtek.com>
2022-08-02 14:46:30 +0000

    algunos ajustes esteticos

--- webapp/controller/Home.controller.js
+++ webapp/controller/Home.controller.js
@@ -11,29 +11,42 @@ sap.ui.define([
         formatter: formatter,
 
         onInit: function () {
+            oController = this;
+
+            var oRootPath = jQuery.sap.getModulePath("softtek.sostenibilidadmenu");
             var oViewModel = new JSONModel({
-                isPhone: Device.system.phone
+                isPhone: Device.system.phone,
+                selectedMenuItem: "I",
+                rootPath: oRootPath
             });
             this.getView().setModel(oViewModel, "view");
+
+            //ODS Model
+            var oODSModel = new JSONModel({});
+            this.getView().setModel(oODSModel, "ODSModel");
+
             Device.media.attachHandler(function (oDevice) {
                 this.getModel("view").setProperty("/isPhone", oDevice.name === "Phone");
             }.bind(this));
-            this.oView = this.getView();
+        },
+        
+        onSelectItem: function (oEvent) {
+			var sKey = oEvent.getParameter("item").getKey();
+			this.byId("pageContainer").to(this.getView().createId(sKey));
+		},
+
+        onNavODS4: function(){
+            var sRootPath = oController.getModel("view").getProperty("/rootPath");
+            oController.getModel("ODSModel").setData({
+                titulo: "Objetivo 4",
+                subTitulo: "Educación de calidad",
+                icono: sRootPath + "/images/IconDetailODS4.png",
+            });
+            oController._navToDetail();
         },
 
-        onAfterRendering: function (oEvent) {
-			var oRootPath = jQuery.sap.getModulePath("softtek.sostenibilidadmenu");
-
-			var sPath = oRootPath;
-			if (!sap.ui.Device.system.phone) {
-				sPath = sPath + "/images/homeImage.jpg";
-				this.byId("imgBanner2").setSrc(sPath);
-				//this.byId("imgBanner2").setHeight("30rem");
-			} else {
-				sPath = sPath + "/images/homeImage_small.jpg";
-				this.byId("imgBanner2").setSrc(sPath);
-			}
-
-		}
+        _navToDetail: function(){
+            oController.byId("pageContainer").to(oController.createId("O"));
+        }
     });
 });
